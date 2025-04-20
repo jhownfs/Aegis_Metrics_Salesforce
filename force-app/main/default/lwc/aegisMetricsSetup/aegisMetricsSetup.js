9321c096a6c03bcf3d31bbaa394b562a2bb0fc37
jhonata santos <jhown_fs@hotmail.com>
2025-04-20 19:20:54 -0300

    chore: update event monitoring type to ingest

--- force-app/main/default/lwc/aegisMetricsSetup/aegisMetricsSetup.js
+++ force-app/main/default/lwc/aegisMetricsSetup/aegisMetricsSetup.js
@@ -1,7 +1,10 @@
 import { LightningElement } from 'lwc';
 import setupEvent from '@salesforce/label/c.Aegis_Metrics_Setup_Events';
 import defaultEvent from '@salesforce/label/c.Aegist_Metrics_Default_Events';
+import toggleActive from '@salesforce/label/c.Aegis_Metrics_Toggle_Active';
+import toggerInative from '@salesforce/label/c.Aegis_Metrics_Toggle_Inative';
 import getMonitoringTypes from '@salesforce/apex/AegisMetricsServices.getMonitoringTypes';
+import updateMonitoringTypes from '@salesforce/apex/AegisMetricsServices.updateEventTypes';
 
 export default class aegisMetricsSetup extends LightningElement {
  
@@ -10,7 +13,9 @@ export default class aegisMetricsSetup extends LightningElement {
 
   label = {
     setupEvent,
-    defaultEvent
+    defaultEvent,
+    toggleActive,
+    toggerInative
   };
 
   async connectedCallback() {
@@ -22,4 +27,36 @@ export default class aegisMetricsSetup extends LightningElement {
       console.error('Error fetching monitoring types:', error);
     }
   }
+
+  handleToggle(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    const monitoringTypeId = event.target.value;
+    const isEnabled = event.target.checked;
+
+    const Event_Monitoring_Type__mdt = {
+      Id: monitoringTypeId,
+      Enabled_To_Ingest__c: isEnabled
+    };
+
+    this.handleSave(Event_Monitoring_Type__mdt);
+    // Update the local state   
+    // Call Apex method to update the monitoring type status
+    // You can implement the logic to call the Apex method here
+    console.log(`Monitoring Type ID: ${monitoringTypeId}, Enabled: ${isEnabled}`);
+  }
+
+  async handleSave(eventType) {
+
+   const eventTypeJson = JSON.stringify(eventType);
+   console.log('Event Type JSON:', eventTypeJson);
+   const eventUpdate = await updateMonitoringTypes({ jsonData: eventTypeJson });
+
+    if (eventUpdate) {
+        console.log('Event updated successfully:', eventUpdate);
+      } else {
+        console.error('Error updating event:', eventType);
+      }
+  }
+
 }
